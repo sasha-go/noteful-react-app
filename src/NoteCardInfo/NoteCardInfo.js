1135fceb22d1dd659fd7e8968d11aff69fa26f83
sasha-go <sasha.lfg@gmail.com>
2020-03-07 14:30:56 -0800

    added formatDate function to make modified dates more friendly

--- src/NoteCardInfo/NoteCardInfo.js
+++ src/NoteCardInfo/NoteCardInfo.js
@@ -3,20 +3,40 @@ import { Link } from 'react-router-dom';
 import './NoteCardInfo.css';
 import NoteContext from '../NoteContext';
 import '../App.css'
-import NotePageSidebar from '../NotePageSidebar/NotePageSidebar';
+
+//https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date 
+function formatDate(date) {
+
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return monthNames[monthIndex] + ' ' + day + ', ' + year;
+}
+
 
 class NoteCardInfo extends Component {
+  
   static contextType = NoteContext; 
 
-  
+
   render() {
+
+    const modified = formatDate(new Date(this.props.modified));
 
     return (
       <>
         {/* Why isn't context working here? */}
         <Link to={`/notes/${this.props.id}`} className="note-card-title">{this.props.name}</Link>
         <div className="NoteCardInfo">
-          <p>Last modified: {this.props.modified}</p>
+          <p>Last modified: {modified}</p>
           <button>Delete Note</button>
         </div>
       </>
--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -3,6 +3,21 @@ import './NotePageMain.css';
 import NoteCardInfo from '../NoteCardInfo/NoteCardInfo';
 import NoteContext from '../NoteContext';
 
+function formatDate(date) {
+
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return monthNames[monthIndex] + ' ' + day + ', ' + year;
+}
 
 class NotePageMain extends Component {
   static contextType = NoteContext;
@@ -13,6 +28,8 @@ class NotePageMain extends Component {
       note => note.id === this.props.match.params.noteId
       )
 
+
+    const modified = formatDate(new Date(this.props.name))
     return(
       <>
         <div className="NotePageMain">
