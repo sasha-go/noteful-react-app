0e77d945a0fdb5d32521b6250c9286b2413cd6d8
sasha-go <sasha.lfg@gmail.com>
2020-03-11 20:10:27 -0700

    added html for NewFolder and NewNote

---
+++ src/NewFolder/NewFolder.js
@@ -0,0 +1,28 @@
+import react from 'react';
+
+class NewFolder extends React.Component {
+
+	render() {
+		return (
+			<>
+				<section>
+					<h2>Create a Folder</h2>
+						<label for="folder-name-field">Folder Name:</label>
+						<input type="text"></input>
+						<button type="submit">Create Folder</button>
+					<form>
+
+					</form>
+				</section>
+			</>
+		)
+	}
+}
+
+export default NewFolder;
+
+
+// Create a new component AddFolder that implements a form to capture the name of a new folder from the user. 
+// This form should submit the name of the new folder to the POST /folders endpoint on the server.
+// Ensure that any errors are properly handled. 
+// Add a button to the navigation to invoke the new form.
--- src/NewNote/NewNote.js
+++ src/NewNote/NewNote.js
@@ -1,7 +1,34 @@
 import react from 'react';
 
+class NewNote extends React.Component {
+
+	render() {
+		return (
+			<>
+				<section>
+					<h2>Create a Note</h2>
+					<form>
+						<label for="note-name-field">Name</label>
+						<input type="text"></input>
+						<label for="content-name-field">Note Content</label>
+						<input type="text"></input>
+						<label for="folder-dropdown-field">Folder</label>
+						<input type="text"></input>
+						<button type="submit">Add Note</button>
+					</form>
+				</section>
+			</>
+		)
+	}
+}
 
 
 
 
 export default NewNote;
+
+// Create a new component AddNote that implements a form to capture the name, content and folder for a new Note. 
+//Submit to the POST /notes endpoint on the server. Add validation to ensure that the name of the note is not left blank. 
+// The folder should be selected from a list of existing folders. 
+//Ensure that errors are properly handled. 
+// Add a button to the note list page to invoke this new form.
